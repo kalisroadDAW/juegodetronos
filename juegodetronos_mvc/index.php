5a5592fbbb27aca5f4995e3c0096e8994bb830c1
kalisroadDAW <82877300+kalisroadDAW@users.noreply.github.com>
2022-02-13 21:15:25 +0100

    Add files via upload

---
+++ juegodetronos_mvc/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Personajes de Juego de Tronos</title>
+   </head>
+   
+   <link rel="stylesheet" href="estilo.css">
+
+   
+   <body>
+
+   
+
+     <div class="container">
+       <a href="actores.php" class="imagen" target="_blank" ><img src="f1.png"><p>actores</p></a>
+       <a href="actoresTemp.php" class="imagen" target="_blank" ><img src="f2.png"><p>Capítulos</p></a>
+     </div>
+
+  
+
+ 
+
+   </body>
+
+
+</html>
